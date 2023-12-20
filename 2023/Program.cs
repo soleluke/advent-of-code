14aac9a83e680fb4942f6f506f239c3d270bae73
Luke Ebersole <ebersoleluke@gmail.com>
2023-12-19 15:19:41 -0900

    fix format

--- 2023/Program.cs
+++ 2023/Program.cs
@@ -29,7 +29,7 @@ public class Program
       {11,new Day11()},
       {12,new Day12()},
       {13,new Day13()},
-      {14, new Day14()},
+      {14,new Day14()},
       {15,new Day15()},
       {16,new Day16()},
       {17,new Day17()},
