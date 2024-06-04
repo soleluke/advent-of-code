72cc5e6ead1ef0695554a7e94535c8f17a994e14
Luke Ebersole <ebersoleluke@gmail.com>
2024-06-04 12:17:03 -0800

    20921 day 1

--- 2021/Day01.cs
+++ 2021/Day01.cs
@@ -8,18 +8,34 @@ public class Day01 : IDay
 
   public void Run(string input)
   {
+    var depths = ParseRows(input);
+    int increases = 0;
+    int cur = 0;
+    int windowSize = 3;
+    while (cur < depths.Count() - windowSize)
+    {
+      int windowA = depths.Skip(cur).Take(windowSize).Sum();
+      int windowB = depths.Skip(cur + 1).Take(windowSize).Sum();
+      if (windowB > windowA)
+        increases++;
+      cur++;
+    }
+    Console.WriteLine(increases);
   }
 
-  public void ParseRows(string input)
+  public int[] ParseRows(string input)
   {
+    IEnumerable<int> depths = new List<int>();
 
     using (StringReader sr = new StringReader(input))
     {
       string? line;
       while ((line = sr.ReadLine()) != null && !string.IsNullOrWhiteSpace(line))
       {
+        depths = depths.Append(int.Parse(line));
       }
 
     }
+    return depths.ToArray();
   }
 }
