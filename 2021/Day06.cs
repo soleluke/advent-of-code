6acbe322b7b20c434726511910d37669c9456987
Luke Ebersole <ebersoleluke@gmail.com>
2024-06-05 12:19:33 -0800

    2021 day 6

--- 2021/Day06.cs
+++ 2021/Day06.cs
@@ -8,18 +8,47 @@ public class Day06 : IDay
 
   public void Run(string input)
   {
+    var fish = ParseRows(input);
+    int days = 256;
+    long[] state = new long[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    foreach (var f in fish)
+    {
+      state[f]++;
+    }
+    for (int d = 0; d < days; d++)
+    {
+      //Console.WriteLine(string.Join(',', state));
+      long[] newState = new long[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+      for (int i = 0; i <= 8; i++)
+      {
+        if (i == 0)
+        {
+          newState[6] = state[0];
+          newState[8] = state[0];
+        }
+        else
+        {
+          newState[i - 1] += state[i];
+        }
+      }
+      state = newState;
+    }
+    Console.WriteLine(state.Sum());
   }
 
-  public void ParseRows(string input)
+  public IEnumerable<int> ParseRows(string input)
   {
 
     using (StringReader sr = new StringReader(input))
     {
       string? line;
-      while ((line = sr.ReadLine()) != null && !string.IsNullOrWhiteSpace(line))
+      line = sr.ReadLine();
+      if (!string.IsNullOrWhiteSpace(line))
       {
+        return line.Split(',').Select(s => int.Parse(s));
       }
 
     }
+    throw new Exception("bad input");
   }
 }
