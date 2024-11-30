81695f05241bd4f20c8be198cbf3b49bf9984246
Luke Ebersole <luke@lebersole.com>
2024-11-30 14:51:16 -0900

    initial 2024 commit

---
+++ 2024/Day17.cs
@@ -0,0 +1,25 @@
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections.Immutable;
+using System.Drawing;
+
+public class Day17 : IDay
+{
+
+  public void Run(string input)
+  {
+  }
+
+  public void ParseRows(string input)
+  {
+
+    using (StringReader sr = new StringReader(input))
+    {
+      string? line;
+      while ((line = sr.ReadLine()) != null && !string.IsNullOrWhiteSpace(line))
+      {
+      }
+
+    }
+  }
+}
